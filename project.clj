d57fbe58a6d21e0efd5eb78a3104cc2a96f0edb3
Duncan McGreggor <oubiwann@gmail.com>
2018-03-11 14:48:48 -0500

    Initial files.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hexagram30/common "0.1.0-SNAPSHOT"
+  :description "Code an utilities for common use by hexagram30 projects"
+  :url "https://github.com/hexagram30/common"
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]])
