c8910c53e5b4015040f29d99b00e769f5c477b5d
Duncan McGreggor <oubiwann@gmail.com>
2018-03-11 14:49:22 -0500

    Moved EDN reader code from terminal project.

---
+++ src/hexagram30/common/file.clj
@@ -0,0 +1,9 @@
+(ns hexagram30.common.file
+  (:require
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]))
+
+(defn read-edn-resource
+  [filename]
+  (with-open [rdr (io/reader (io/resource filename))]
+    (edn/read (new java.io.PushbackReader rdr))))
