ea1c956f1f487340061d9f1cc01ebdf0b701b77f
Duncan McGreggor <oubiwann@gmail.com>
2019-01-15 20:23:42 -0500

    Added common components.

---
+++ src/hxgm30/common/components/config.clj
@@ -0,0 +1,70 @@
+(ns hxgm30.common.components.config
+  (:require
+   [com.stuartsierra.component :as component]
+   [hxgm30.common.config :as config]
+   [taoensso.timbre :as log]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn get-cfg
+  [system]
+  (->> [:config :data]
+       (get-in system)
+       (into {})))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Config Component API   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn log-color?
+  [system]
+  (boolean
+    (new Boolean (get-in (get-cfg system) [:logging :color]))))
+
+(defn log-level
+  [system]
+  (get-in (get-cfg system) [:logging :level]))
+
+(defn log-nss
+  [system]
+  (get-in (get-cfg system) [:logging :nss]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Component Lifecycle Implementation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrecord Config [data])
+
+(defn start
+  [this]
+  (log/info "Starting config component ...")
+  (log/debug "Started config component.")
+  this)
+
+(defn stop
+  [this]
+  (log/info "Stopping config component ...")
+  (log/debug "Stopped config component.")
+  (assoc this :data nil))
+
+(def lifecycle-behaviour
+  {:start start
+   :stop stop})
+
+(extend Config
+  component/Lifecycle
+  lifecycle-behaviour)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Component Constructor   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn create-component
+  ""
+  ([]
+    (create-component (config/data)))
+  ([cfg-data]
+    (log/trace "Built configuration:" cfg-data)
+    (map->Config {:data cfg-data})))
