a72559a622515d57e009e37d56fc926b163cbb18
Duncan McGreggor <oubiwann@gmail.com>
2020-01-05 21:33:19 -0600

    Bumped version for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hexagram30/common "0.2.0-SNAPSHOT"
+(defproject hexagram30/common "0.2.0"
   :description "Code and utilities for use by all hexagram30 projects"
   :url "https://github.com/hexagram30/common"
   :license {
